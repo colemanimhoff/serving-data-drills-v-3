bbaf4a2140a0e5cc63c88c7649464a430ec8ebef
Coleman Imhoff <colemanimhoff@gmail.com>
2018-05-01 12:16:49 -0600

    deployed to heroku

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = parseInt(process.env.PORT || 3000)
+
+const students = require('./students.js')
+
+function getDataById(data, id) {
+    return data.filter(currentItem => currentItem.id == id)
+}
+
+app.get('/', (request, response) => response.json(students))
+
+app.get('/:id', (request, response) => {
+    let filteredStudent = getDataById(students, request.params.id)
+    if (filteredStudent.length < 1) {
+        response.status(404).json({
+            error: { message: 'No record found!' }
+        })
+    } else {
+        response.json({ student: filteredStudent })
+    }
+})
+
+app.listen(port)
+    .on('error', console.error.bind(console))
+    .on('listening', console.log.bind(console, `Listening on ${port}`))
